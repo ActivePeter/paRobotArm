1168cf9d68f0d044ed47001a0f3754d80791ff28
ActivePeter <1020401660@qq.com>
2021-06-15 13:56:36 +0800

    是

--- components/paLibSubs/Conf_ILI9341.h
+++ components/paLibSubs/Conf_ILI9341.h
@@ -1,6 +1,6 @@
 #ifndef Conf_ILI9341_h_____
 #define Conf_ILI9341_h_____
-#define Conf_ILI9341_CS
-#define Conf_ILI9341_DC
-#define Conf_ILI9341_RST
+#define Conf_ILI9341_CS GPIO_NUM_0
+#define Conf_ILI9341_DC GPIO_NUM_0
+#define Conf_ILI9341_RST GPIO_NUM_0
 #endif
